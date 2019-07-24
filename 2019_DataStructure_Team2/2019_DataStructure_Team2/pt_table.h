edb0c5bae4c4584fee6969b14183ba94577d6da0
ckdgus0505 <ckdgus0505@gmail.com>
2019-07-24 16:11:52 +0900

    separate 12 & 19

--- 2019_DataStructure_Team2/2019_DataStructure_Team2/pt_table.h
+++ 2019_DataStructure_Team2/2019_DataStructure_Team2/pt_table.h
@@ -1,4 +1,6 @@
 // Q6, Q12, Q19를 해결하기 위한 함수
 
 void Q6(int num, member_info table); // 가장 오래된 회원(num)명 만큼 출력해준다.
-void pt(member_info table);
+void pt(member_info &table);
+void Q12(member_info &table);
+void Q19(member_info &table);
